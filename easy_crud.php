bb886b6b9e0b3caaca10912363ab68d7ee00e0ae
Ankit-Kulkarni <ankit.kul1890@gmail.com>
2014-05-11 18:51:34 +0530

    overloaded the insert function to perform 2 kinds of queries, with 2 parameters in normal and if condition is also there then need to pass 3 parameters

--- easy_crud.php
+++ easy_crud.php
@@ -42,105 +42,9 @@ THE SOFTWARE.
 <!-- including the connection file for database. -->
 <?php include("db_connect.php"); ?>
 
-<!-- $query = "INSERT INTO `User_Info` VALUES('', '$username', '$password')"; -->
-
 <?php
-function ins($table_name, $param) {
-  $table_name;
-  $part1="INSERT INTO `" .$table_name . "` VALUES(" ;  
-  $part2 =  format_param_list($param) ;
-  $query= $part1. $part2;
-  echo $query;
-  echo '<br><br><br>';
-  if(run_query($query)) {
-  	echo "Succefull insertion of data " ;
-  }
-  else {
-  	echo "The username exists. Change it and try again." ;
-  }
 
-}
-
-function format_param_list($param, $param_list){
-# This function takes an Array as input and returns the formated string. Ex- ('value1','value2','value3',...)
-	$part2="(";
-	$max = count($param_list);
-	$quote ="'";
-	if($param == "col"){ $quote = "`" ;}
-	for($s=0; $s<$max; $s++){
-		
-		$part2= $part2. $quote;
-		$part2= $part2. $param_list[$s]. $quote. " ";
-		
-		if($max === ($s + 1)){
-			
-			break;
-		}
-		$part2= $part2. ", ";
-
-	}
-	
-	$part2= $part2. ")";
-	return $part2 ;
-}
-
-function run_query($query) {
-# This function runs the query and return 1 if succesful else return 0.
-	  if ($queryRun =  mysql_query($query)) {
-	  	#code
-	  	return 1;
-	  }
-	  else {
-	  	return 0;
-	  }
-}
-
-$val =  array('', 'John', 'Doe');
-#ins("User_Info", $val ); // call the function
-?>
-
-
-<?php 
-	$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
-	$table_name = "User_Info" ;
-	#$data = array('id' =>'' , 'Username'=>'arrayas' , 'Password'=>'nothing');
-	$data["Id"] = " ";
-	$username = "Username";
-	$password = "Password" ;
-	$username_val = "Doe1";
-	$password_val = "b" ;
-	$data[$username] = $username_val ;
-	$data[$password]  = $password_val ;
-?>
-
-
-<?php
-function ins_with_column_name($table_name,$data) {
-# This function creates the format of the insert query .
-# INSERT INTO table_name (column1,column2,column3,...) VALUES (value1,value2,value3,...)
-	$initial="INSERT INTO `" .$table_name . "` " ;		
-	$query ="";
-	list($col, $val) = seperate_key_values($data) ;
-	$col_val_list = cal_col_val($col, $val);
-	$query = $initial .$col_val_list ;
-
-	echo "<br><br>";		 
-	echo $query;
-	echo "<br>";
-	if(run_query($query)) {
-		echo "Succefull insertion of data " ;
-	}
-	else {
-		echo "The username exists. Change it and try again." ;
-	}
-}
-
-function cal_col_val($col, $val){
-# This function takes formated columns and values and returns the concatenated strings of column names and values in query format.
-# For ex- ('column1','column2','column3', ...) VALUES ('value1','value2','value3', ...) .		
-	$col_val_list = $col. " VALUES " . $val ;
-	return $col_val_list;
-}
+#helpers
 
 function seperate_key_values($data){
 # This function takes assosiative array as input and return array containing formated columns and values
@@ -156,7 +60,125 @@ function seperate_key_values($data){
 	return array($col, $val);
 }
 
+function format_param_list($param, $param_list){
+# This function takes an Array as input and returns the formated string. Ex- ('value1','value2','value3',...)
+	$part2="(";
+	$max = count($param_list);
+	$quote ="'";
+	if($param == "col"){ $quote = "`" ;}
+	for($s=0; $s<$max; $s++){		
+		$part2= $part2. $quote;
+		$part2= $part2. $param_list[$s]. $quote. " ";		
+		if($max === ($s + 1)){			
+			break;
+		}
+		$part2= $part2. ", ";
+	}	
+	$part2= $part2. ")";
+	return $part2 ;
+}
 
-ins_with_column_name($table_name, $data) ;
+function run_query($query) {
+# This function runs the query and return 1 if succesful else return 0.
+	  if ($queryRun =  mysql_query($query)) {
+	  	#code
+	  	return 1;
+	  }
+	  else {
+	  	return 0;
+	  }
+}
+
+?>
+
+
+
+<?php
+function insert() {
+# This variadic function creates the format of the insert query .
+# INSERT INTO table_name (column1,column2,column3,...) VALUES (value1,value2,value3,...)
+
+	for ($i = 0; $i < func_num_args(); $i++) {
+	    #printf("Argument %d: %s\n", $i, func_get_arg($i));
+	    $param[$i]=func_get_arg($i);
+	    
+	}
+	try {
+		$param_len = count($param) ;
+		if ( $param_len < 2 ) {
+			echo "<br> insert with less then 2 parameter"; 
+		} elseif ($param_len == 2) {
+			# code...
+			$table_name = $param[0];
+			$data = $param[1];
+			$condition = "";
+			echo "<br> insert query with 2 param";			
+			run_insert_query($table_name, $data, $condition);		
+
+		} elseif ($param_len == 3) {
+			# code...
+			$table_name = $param[0];
+			$data = $param[1];
+			$condition = $param[2] ;
+			echo " <br> insert query with 3 param";
+			run_insert_query($table_name, $data, $condition);
+			
+		}
+		
+		} catch (Exception $e) {
+			return $e; 	
+		}
+
+		
+	
+}
+
+function run_insert_query($table_name, $data, $condition){
+		try {
+		$initial="INSERT INTO `" .$table_name . "` " ;		
+		$query ="";
+		
+		list($col, $val) = seperate_key_values($data) ;
+		$col_val_list = cal_col_val($col, $val, $condition);
+		$query = $initial .$col_val_list ;
+
+		echo "<br><br>";		 
+		echo $query;
+		echo "<br>";
+		if(run_query($query)) {
+			echo "<br> Succefull insertion of data " ;
+		}
+		else {
+			echo " <br> The username exists. Change it and try again." ;
+		}
+		
+	} catch (Exception $e) {
+		return $e; 	
+	}
+
+
+}
+
+
+function cal_col_val($col, $val, $condition){
+# This function takes formated columns and values and returns the concatenated strings of column names and values in query format.
+# For ex- ('column1','column2','column3', ...) VALUES ('value1','value2','value3', ...) .		
+	
+	$cond = "WHERE ". $condition;
+	echo " <br>This is \$cond -   " . $cond ;
+	if($condition == ""){
+		$col_val_list = $col. " VALUES " . $val ;
+		
+	}
+	else{
+		$col_val_list = $col. " VALUES " . $val ." " . $cond ;
+	}
+	
+	return $col_val_list;
+}
+
+
+
+
 
 ?>
